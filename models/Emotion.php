88998c22010772dab4b822ca01c73e0fb5cceb04
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-09 23:01:53 +0200

    commit emotion

--- models/Emotion.php
+++ models/Emotion.php
@@ -18,9 +18,85 @@ class Emotion
         $this->la_colere = $_la_colere;
         $this->le_degout = $_le_degout;
         $this->la_surprise = $_la_surprise;
-        $this->la_mepris = $_le_mepris;
+        $this->le_mepris = $_le_mepris;
         $this->la_tristesse = $_la_tristesse;
         $this->la_joie = $_la_joie;
         $this->la_peur = $_la_peur;
+    }
+
+    /////////////////SET/////////////////////////
+
+    public function setLaColere($_la_colere)
+    {
+        $this->la_colere = $_la_colere;
+    }
+
+    public function setLeDegout($_le_degout)
+    {
+        $this->le_degout = $_le_degout;
+    }
+
+    public function setLaSurprise($_la_surprise)
+    {
+        $this->la_surprise = $_la_surprise;
+    }
+
+    public function setLaTristesse($_la_tristesse)
+    {
+        $this->la_tristesse = $_la_tristesse;
+    }
+
+    public function setLaJoie($_la_joie)
+    {
+        $this->la_joie = $_la_joie;
+    }
+
+    public function setLeMepris($_le_mepris)
+    {
+        $this->le_mepris = $_le_mepris;
+    }
+
+    public function setLaPeur($_la_peur)
+    {
+        $this->la_peur = $_la_peur;
+    }
+
+    //////////////////////GET///////////////////////
+
+
+    public function getLaColere()
+    {
+
+        return $this->la_colere;
+    }
+
+    public function getLeDegout()
+    {
+        return $this->le_degout;
+    }
+
+    public function getLaSurprise()
+    {
+        return $this->la_surprise;
+    }
+
+    public function getLaTristesse()
+    {
+        return $this->la_tristesse;
+    }
+
+    public function getLaJoie()
+    {
+        return $this->la_joie;
+    }
+
+    public function getLeMepris()
+    {
+        return $this->le_mepris;
+    }
+
+    public function getLapeur()
+    {
+        return $this->la_peur;
     }
 }
