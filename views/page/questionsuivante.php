a332fe985e702494a20214d9fe8995c0b4fbdfd8
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-11 16:12:24 +0200

    new commit

--- views/page/questionsuivante.php
+++ views/page/questionsuivante.php
@@ -1,13 +1,9 @@
-
-
-
-
 <div class="containerpq">
-        <div class="h1Divpq">
-            <h3 class="titre1pq">Quelle émotion avez-vous ressenti? </h3>
-            <a class="apq" href="?section=accueil"> question précédente</a>
-        </div>
-<!-- 
+    <div class="h1Divpq">
+        <h3 class="titre1pq">Quelle émotion avez-vous ressenti? </h3>
+        <a class="apq" href="?section=accueil"> question précédente</a>
+    </div>
+    <!-- 
         Joie
         Tristesse -- 
         Surprise --
@@ -16,18 +12,16 @@
         Dégoût --
         Mépris  -->
 
-        <div class="imageDivpq">
+    <div class="imageDivpq">
 
-            <div><img class="imagepq" src="public/images/images2page/killbill.jpg" alt=""></div>
-            <div><img class="imagepq" src="public/images/images2page/oldboy.jpg" alt=""></div>
-            <div><img class="imagepq" src="public/images/images2page/seven.jpg" alt=""></div>
-            <div><img class="imagepq" src="public/images/images2page/joker.jpg" alt=""></div>
-            <div><img class="imagepq" src="public/images/images2page/greenmile.jpg" alt=""></div>
-            <div><img class="imagepq" src="public/images/images2page/intouchables.jpg" alt=""></div>
-            <div><img class="imagepq" src="public/images/images2page/shining.jpg" alt=""></div>
-        </div>
-
-       
+        <div><img class="imagepq" src="public/images/images2page/killbill.jpg" alt=""></div>
+        <div><img class="imagepq" src="public/images/images2page/oldboy.jpg" alt=""></div>
+        <div><img class="imagepq" src="public/images/images2page/seven.jpg" alt=""></div>
+        <div><img class="imagepq" src="public/images/images2page/joker.jpg" alt=""></div>
+        <div><img class="imagepq" src="public/images/images2page/greenmile.jpg" alt=""></div>
+        <div><img class="imagepq" src="public/images/images2page/intouchables.jpg" alt=""></div>
+        <div><img class="imagepq" src="public/images/images2page/shining.jpg" alt=""></div>
     </div>
+
 
 </div>
