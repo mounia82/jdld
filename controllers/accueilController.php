0f394bd7e2081d9a1cabbf5c300dd715ed2ba7ca
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-12 15:56:58 +0200

    new commit

--- controllers/accueilController.php
+++ controllers/accueilController.php
@@ -3,7 +3,9 @@
     if(isset($_GET["categ"]) && isset($_GET["emotion"]))
     {
         //ici contact ta base de donnée, sur base des deuxelement que tu as ici au dessus 
+        
         //tu crée une petite variable avec ta réplique et tu l'affiche dans acceuil.php
+        
     }
 
 
