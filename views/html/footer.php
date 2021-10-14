cdd7effc8849b8bd4abb629d9b372699ae432285
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-14 23:02:24 +0200

    commit icons

--- views/html/footer.php
+++ views/html/footer.php
@@ -2,12 +2,17 @@
 
 
  <footer>
-
-
+ <div class="icons">
+        <i class="bi bi-facebook" style="color: rgb(253, 253, 252); "></i>
+        <i class="bi bi-instagram" style="color: blanchedalmond;"></i>
+        <i class="bi bi-linkedin" style="color: blanchedalmond;"></i>
+    </div>
+<!-- 
     <div class="media">
 
         <a href="https://www.facebook.com/">
-            <img class="face" src="public/images/logos/logo.png" alt="facebook"></a>
+            <img class="face" src="public/images/logos/facebook.png" alt="facebook"></a>
+            <i class="bi bi-instagram"></i>
 
         <a href="https://twitter.com/">
             <img class="tweet" src="public/images/twitter.png" alt="tweeter"></a>
@@ -15,7 +20,7 @@
         <a href="https://www.instagram.com/">
             <img class="insta" src="public/images/instagram.png" alt="instagram"></a>
 
-    </div>
+    </div> -->
     <p class="copy"> &copy;Copyright 2021</p>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- views/html/head.php
+++ views/html/head.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewsport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <!-- <link rel="stylesheet" href="public/css/styleformulaires.css"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Spartan&display=swap" rel="stylesheet">
