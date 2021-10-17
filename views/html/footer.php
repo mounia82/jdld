9fdc950fb4520c4eae78eeef14895a26938d55d7
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-17 20:19:29 +0200

    new

--- views/html/footer.php
+++ views/html/footer.php
@@ -4,20 +4,7 @@
         <i class="bi bi-instagram" style="color: rgb(253, 253, 252); "></i>
         <i class="bi bi-linkedin" style="color: rgb(253, 253, 252); "></i>
     </div>
-    <!-- 
-    <div class="media">
 
-        <a href="https://www.facebook.com/">
-            <img class="face" src="public/images/logos/facebook.png" alt="facebook"></a>
-            <i class="bi bi-instagram"></i>
-
-        <a href="https://twitter.com/">
-            <img class="tweet" src="public/images/twitter.png" alt="tweeter"></a>
-
-        <a href="https://www.instagram.com/">
-            <img class="insta" src="public/images/instagram.png" alt="instagram"></a>
-
-    </div> -->
     <p class="copy"> &copy;Copyright 2021</p>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -31,6 +18,5 @@
 
 
     </body>
-
     </html>
 </footer>
