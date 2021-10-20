0ea822b98759f20ab6b78fa2ca0a029513e1c577
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-20 22:55:03 +0200

    new commit

--- controllers/router.php
+++ controllers/router.php
@@ -1,68 +1,63 @@
 <?php
 if (isset($_GET["section"])) {
     // il est connecté
-    if(isset($_SESSION["id"])){
-    switch ($_GET["section"]) {
-        case 'accueil':
-            include("controllers/accueilController.php");
-            break;
-        case 'apropos':
-            include("controllers/accueilController.php");
-            break;
-        case 'contact':
-            include("controllers/contactController.php");
-            break;
-        case 'connexion':
-            include("controllers/connexionController.php");
-            break;
-        case 'inscription':
-            include("controllers/inscriptionController.php");
-            break;
-        case 'questionsuivante':
-            include("controllers/questionsuivanteController.php");
-            break;
-        case 'replique':
-            include("controllers/repliqueController.php");
-            break;
-        case 'deconnexion':
-            include("controllers/deconnexionController.php");
-            break;
-        default:
-            include("views/error/404.php");
-    }
-} 
-
-
-else{
-    // il n'est pas connecté
-    switch($_GET["section"]){
-        case 'accueil' :
-            include("controllers/accueilController.php");
-            break;
-        case 'apropos' :
-            include("controllers/aproposController.php");
-            break;
-        case 'categorie' :
-            include("controllers/categorieController.php");
-            break;
-        case 'contact' :
-            include("controllers/contactController.php");
-            break;
-        case 'connexion' :
-            include("controllers/connexionController.php");
-            break;
-        case 'inscription' :
-            include("controllers/inscriptionController.php");
-            break;
-
-        default:
-            include("views/error/404.php");
-
+    if (isset($_SESSION["id"])) {
+        switch ($_GET["section"]) {
+            case 'accueil':
+                include("controllers/accueilController.php");
+                break;
+            case 'apropos':
+                include("controllers/accueilController.php");
+                break;
+            case 'contact':
+                include("controllers/contactController.php");
+                break;
+            case 'connexion':
+                include("controllers/connexionController.php");
+                break;
+            case 'inscription':
+                include("controllers/inscriptionController.php");
+                break;
+            case 'questionsuivante':
+                include("controllers/questionsuivanteController.php");
+                break;
+            case 'replique':
+                include("controllers/repliqueController.php");
+                break;
+            case 'deconnexion':
+                include("controllers/deconnexionController.php");
+                break;
+            default:
+                include("views/error/404.php");
+        }
+    } else {
+        // il n'est pas connecté
+        switch ($_GET["section"]) {
+            case 'accueil':
+                include("controllers/accueilController.php");
+                break;
+            case 'apropos':
+                include("controllers/accueilController.php");
+                break;
+            case 'questionsuivante':
+                include("controllers/questionsuivanteController.php");
+                break;
+            case 'replique':
+                include("controllers/repliqueController.php");
+                break;
+            case 'contact':
+                include("controllers/contactController.php");
+                break;
+            case 'connexion':
+                include("controllers/connexionController.php");
+                break;
+            case 'inscription':
+                include("controllers/inscriptionController.php");
+                break;
+            default:
+                include("views/error/404.php");
         }
     }
-
-}
-
-else {
+} else {
     include("controllers/accueilController.php");
 }
