d3e35467617c30e8872fcc6c59436059fb6cfc76
Iman Chamileva <iman.moussaevna@gmail.com>
2021-10-20 21:38:09 +0200

    new

--- views/page/accueil.php
+++ views/page/accueil.php
@@ -67,7 +67,7 @@ if (isset($_GET["categ"]) && isset($_GET["emotion"])) {
 
                 <p class="aproposTexte" data-aos="fade-left" data-aos-duration="1800" data-aos-easing="ease-in-out" data-aos-delay="300">
                     Devenir un as de la répartie? Mais comment ? <br>
-                    Alors, c'est très simple. Il suffit de scroller en bas et choisir la catégorie et l'émotion ressentie lors de votre discussion afin qu'une réplique ??? soit générée! Deux répliques s'appliqueront par catégorie et émotion. Vous n'y trouvez pas votre compte ? On a pensé à tout: choisissez autre dans les deux champs et le tour est joué!
+                    C'est très simple. Il suffit de scroller en bas et choisir la catégorie et l'émotion ressentie lors de votre discussion afin qu'une réplique ??? soit générée! Deux répliques s'appliqueront par catégorie et émotion. Vous n'y trouvez pas votre compte ? On a pensé à tout: choisissez autre dans les deux champs et le tour est joué!
                     N'oubliez pas le second degré lorsque la réplique vous est présentée, parce que nous, nous l'avons pas oublié :)
 
                 </p>
